85396f1b893fe1673eb87fd740fd58cbf1ffe4a1
Robert Wang <wangzihang008>
2018-11-30 10:51:01 +0800

    src/test/java/com/fdmgroup/Controllers/DashboardCustomerTest.java is acting strange

--- src/test/java/com/fdmgroup/Controllers/DashboardCustomerTest.java
+++ src/test/java/com/fdmgroup/Controllers/DashboardCustomerTest.java
@@ -1,26 +1,18 @@
 package com.fdmgroup.Controllers;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.junit.Test;
-import org.springframework.ui.Model;
 
-public class DashboardCustomerTest {
+public class DashBoardCustomerTest {
 
 	@Test
-	public void TestDashBoardCustomer() {
-		DashBoardCustomer db = new DashBoardCustomer();
-		HttpServletRequest request = mock(HttpServletRequest.class);
-		HttpSession session = mock(HttpSession.class);
-		when(request.getSession()).thenReturn(session);
-		when(session.getAttribute("userName")).thenReturn(null);
-		Model model = null;
-		String url = db.gotoDashBoardCustomer(request, model);
-		assertEquals(url, "index");
+	public void GoToDashboard_return_dashBoardCustomer() {
 		
+		DashBoardCustomer dc = new DashBoardCustomer();
+		String nextPage = dc.gotoDashBoardCustomer();
+		
+		assertEquals("dashBoardCustomer", nextPage);
 	}
+	
 }
